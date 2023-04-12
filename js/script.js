2b2e74cf0d81fd5aad103cd1ef07409264fce7bf
Lisa T <llmt2007@aol.com>
2023-04-12 18:44:38 -0400

    Stuck on how to use sticky header and hamburger menu

--- js/script.js
+++ js/script.js
@@ -1,18 +1,32 @@
-const x = document.getElementById("myLinks");
+
 const hamburgerIcon = document.querySelector("i")
 const mediaQuery = window.matchMedia('(max-width: 600px)');
 
 //console.log(mediaQuery)
 
-function myFunction() {
-  
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-}
 
+
+window.onscroll = function() {myFunction()};
+			
+			var header = document.querySelector("header");
+			var sticky = header.offsetTop;
+			
+			function myFunction() {
+			  if (window.pageYOffset > sticky) {
+				header.classList.add("sticky");
+			  } else {
+				header.classList.remove("sticky");
+			  }
+			}
+
+      function myFunction() {
+        var x = document.getElementById("myLinks");
+        if (x.style.display === "block") {
+          x.style.display = "none";
+        } else {
+          x.style.display = "block";
+        }
+      }
 
 /*hamburgerIcon.addEventListener("click", ()=>{
     if (myLinks.style.display === "flex") {
