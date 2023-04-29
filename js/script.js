417d376e7549e9fa2230355bc8289c253d2b879b
Lisa T <llmt2007@aol.com>
2023-04-29 13:47:15 -0400

    Deleted unused code

--- js/script.js
+++ js/script.js
@@ -1,21 +1,5 @@
 const navLinks = document.getElementById("myLinks");
-const hamburgerIcon = document.querySelector("i")
-const mediaQuery = window.matchMedia('(min-width: 768px)');
-const mobileMediaQuery = window.matchMedia('(max-width: 767px)');
 
-
-/*window.onscroll = function() {myFunction()};
-			
-			var header = document.querySelector("header");
-			var sticky = header.offsetTop;
-			
-			function myFunction() {
-			  if (window.pageYOffset > sticky) {
-				header.classList.add("sticky");
-			  } else {
-				header.classList.remove("sticky");
-			  }
-			}*/
 
       function myFunction() {
        
